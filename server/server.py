cb44b1455f54e2885e2486a9d86fd0ef21a45b81
emrehan <emrehan.tuzun@gmail.com>
2016-03-16 11:35:18 +0200

    Add /forecast endpoint

--- server/server.py
+++ server/server.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
+import sys
 import requests
+import json
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,7 +11,13 @@ def hello():
 
 @app.route("/forecast")
 def get_forecast():
-    return requests.get('https://earthnetworks.azure-api.net/getHourly6DayForecast/data/forecasts/v1/hourly?location=-17.9244,25.8567&locationtype=latitudelongitude&units=english&offset=0&metadata=true&verbose=true&subscription-key=d484f320c70e43528cd85eae0618c45a').content
+    with open('config.json') as config_file:    
+        key = json.load(config_file)['p_key']
+        forecast_endpoint = 'https://earthnetworks.azure-api.net/getHourly6DayForecast/data/forecasts/v1/hourly'
+        parameters = '?locationtype=latitudelongitude&units=english&offset=0&metadata=true&verbose=true'
+        location = '-17.9244,25.8567'
+        request_url = forecast_endpoint + parameters + '&location=' + location + '&subscription-key=' + key
+        return requests.get(request_url).content
 
 if __name__ == "__main__":
     app.run()
