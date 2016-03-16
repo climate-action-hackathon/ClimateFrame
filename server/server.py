49b12cd62168f61784fe52db68abcd0e3b90f0b8
emrehan <emrehan.tuzun@gmail.com>
2016-03-16 14:42:08 +0200

    Move pulse api to a new file

--- server/server.py
+++ server/server.py
@@ -1,23 +1,16 @@
-from __future__ import print_function
-import sys
 import requests
 import json
 from flask import Flask
+from apis import pulse
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "ClimateFrame FTW!"
 
 @app.route("/forecast")
 def get_forecast():
-    with open('config.json') as config_file:    
-        key = json.load(config_file)['p_key']
-        forecast_endpoint = 'https://earthnetworks.azure-api.net/getHourly6DayForecast/data/forecasts/v1/hourly'
-        parameters = '?locationtype=latitudelongitude&units=english&offset=0&metadata=true&verbose=true'
-        location = '-17.9244,25.8567'
-        request_url = forecast_endpoint + parameters + '&location=' + location + '&subscription-key=' + key
-        return requests.get(request_url).content
+    return pulse.get_hourly_6day_forecast()
 
 if __name__ == "__main__":
     app.run()
