ba877377e7a5c39a27f2c2859ba5f5234d897ff2
emrehan <emrehan.tuzun@gmail.com>
2016-03-17 00:58:59 +0200

    Update config.json

--- server/apis/pulse.py
+++ server/apis/pulse.py
@@ -4,7 +4,7 @@ import json
 def get_hourly_6day_forecast(latitude, longitude):
     with open('../config.json') as config_file:
         print('get_hourly_6day_forecast()')
-        key = json.load(config_file)['p_key']
+        key = json.load(config_file)['keys']['p_key']
         forecast_endpoint = 'https://earthnetworks.azure-api.net/getHourly6DayForecast/data/forecasts/v1/hourly'
         parameters = '?locationtype=latitudelongitude&units=metric&offset=0&metadata=true&verbose=true'
         # location = '-17.9244,25.8567'
